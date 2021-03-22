2423c867cbd48293c36a941acf4bea133ea40fde
felixgro <me@felixgrohs.dev>
2021-03-22 12:25:08 +0100

    add auto-resize for canvas

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -4,6 +4,11 @@ export const initCanvas = (): CanvasRenderingContext2D => {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
+	window.onresize = () => {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	};
+
 	const ctx = canvas.getContext('2d');
 	if (!ctx) throw new Error('Cannot get CanvasRenderingContext2D');
 
